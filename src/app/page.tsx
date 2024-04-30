6d09daf37e7485b458fd944ee2e51a9c54d0a23f
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-04-30 16:34:50 +0600

    Update page.tsx to include "Mind The Blog" in the div element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="flex flex-col justify-evenly items-center min-h-screen"></div>
+    <div className="flex flex-col justify-evenly items-center min-h-screen">Mind The Blog</div>
   );
 }
